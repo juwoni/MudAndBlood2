434f17f4d103637a06edb1dac528db136b126f9f
juwoni <juwonii@gmail.com>
2026-03-22 19:33:42 +0900

    init

---
+++ Source/MudAndBlood/MudAndBlood.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MudAndBlood.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MudAndBlood, "MudAndBlood" );
+DEFINE_LOG_CATEGORY(LogMudAndBlood);
