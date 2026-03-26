459b748d16269e7972cd91d4bc578dfd552d82c5
juwoni <juwonii@gmail.com>
2026-03-27 01:43:19 +0900

    AMBCharacter 클래스 SandboxCharacter_CMC에 연결

--- Source/MudAndBlood/AMBCharacter.cpp
+++ Source/MudAndBlood/AMBCharacter.cpp
@@ -6,6 +6,7 @@
 // Sets default values
 AAMBCharacter::AAMBCharacter()
 {
+	
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
